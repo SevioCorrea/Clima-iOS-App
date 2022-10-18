14dfaf9bc32dd87953a59c1fcc6cfa94e2d56ff5
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-10-18 17:34:18 -0300

    New lines

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -12,22 +12,37 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchTextField: UITextField! // Campo de texto de pesquisa
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchTextField.delegate = self
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self // Aqui está dizendo que o searchTextField deve retornar ele mesmo. Irá notificar o Controller.
+    }
+    
+
+    @IBAction func searchPressed(_ sender: UIButton) { // Botão Lupa (search) da busca.
+        searchTextField.endEditing(true)
+        print(searchTextField.text!) // Com ! porque nesse caso o text é um optional. Então é um campo vazio por padrão.
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print(searchTextField.text!)
-        return true
+        searchTextField.endEditing(true)
+        print(searchTextField.text!) // Com ! porque nesse caso o text é um optional. Então é um campo vazio por padrão.
+        return true // Para dizer ao textField que ele deve processar esse método (func).
     }
-
-    @IBAction func searchPressed(_ sender: UIButton) {
-       print(searchTextField.text!)
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool { // "Should End Editing"
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Digite a Cidade"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = "" // "Did End Editing" Para limpar o campo de pesquisa quando pressionar "ir".
     }
     
 }
