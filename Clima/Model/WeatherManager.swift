ba3486a952826b713b2e9138a0ce5b5b43384c7d
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-10-24 16:12:29 -0300

    Changes

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -23,21 +23,20 @@ struct WeatherManager {
             // 2. Create a URL Session
             let session = URLSession(configuration: .default)
             // 3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                }
+            }
             // 4. Start the Task
             task.resume()
-        }
-    }
-    
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
         }
     }
     
